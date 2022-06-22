f6e813872edd3b36126affd36923835de79534d3
Harvinder Singh <60150871+harvygr8@users.noreply.github.com>
2022-06-22 22:30:09 +0530

    Updated to 0.3
    - Code Refactoring
    - Using Relative Paths

--- wnow.js
+++ wnow.js
@@ -1,52 +1,21 @@
 #!/usr/bin/env node
 
 const cprl = require('caporal');
-const fetch = require('node-fetch');
 const fs = require('fs');
+const path = require('path');
 const chalk = require('chalk');
 const Table = require('cli-table');
-const {isValidCoords} = require('./utils');
+const {weatherCodes , fetchCurrWeather , isValidCoords} = require('./utils');
 
-
-const prgVersion = 0.2;
+const prgVersion = 0.3;
 cprl.version(prgVersion);
 
-const weatherCodes={
-  0:'Clear Sky',
-  1:'Mainly Clear',
-  2:'Partly Cloudy',
-  3:'Overcast',
-  45:'Fog',
-  51:'Light Drizzle',
-  53:'Moderate Drizzle',
-  55:'Dense Drizzle',
-  61:'Light Rain',
-  63:'Moderate Rain',
-  67:'Heavy Rain',
-  71:'Light Snowfall',
-  73:'Moderate Snowfall',
-  75:'Heavy Snowfall',
-  77:'Snow grain',
-  80:'Light Shower',
-  81:'Moderate Shower',
-  82:'Heavy Shower',
-  85:'Light Snow Shower',
-  86:'Heavy Snow Shower',
-  95:'Thunderstorm',
-  96:'Thunderstorm and Slight Hail',
-  99:'Thunderstorm and Heavy Hail'
-};
-
-const fetchCurrWeather = (lat,long) =>{
-  let url = `https://api.open-meteo.com/v1/forecast?latitude=${parseFloat(lat)}&longitude=${parseFloat(long)}&current_weather=true`;
-  return fetch(url).then((res)=>res.json());
-}
 
 cprl
 .action((args,options)=>{
   let str=""
   let coords = [];
-  fs.readFile('./coords', 'utf8', function(err, data){
+  fs.readFile(path.join(__dirname,'/','coords'), 'utf8', function(err, data){
     if(err){
       let table = new Table();
       table.push([chalk.red.bold(`No co-ordinates found! , use `) + chalk.red.bold("'wnow set <latitude> <longitude>'")]);
@@ -55,20 +24,18 @@ cprl
     }
     str = str.concat(data);
     coords = str.split(' ');
-
     fetchCurrWeather(coords[0],coords[1]).then(apiData=>{
       let table = new Table({
           head:[chalk.white.bold('Temperature'),chalk.white.bold('Wind Speed'),chalk.white.bold('Wind Direction'),chalk.white.bold('Weather')]
         });
+        //add wcode check
         let wcode = weatherCodes[apiData.current_weather.weathercode];
         table.push(
             [chalk.yellow.bold(`${apiData.current_weather.temperature}°C`),chalk.yellow.bold(`${apiData.current_weather.windspeed} km/h`),chalk.yellow.bold(`${apiData.current_weather.winddirection}°`) ,chalk.yellow.bold(`${wcode}`)]
           );
-          console.log("");
-          console.log(chalk.bold(table.toString()));
+        console.log(chalk.bold(table.toString()));
     });
   });
-
 });
 
 cprl
@@ -79,7 +46,7 @@ cprl
 
   if (isValidCoords(args.latitude , args.longitude)){
     let content = `${args.latitude} ${args.longitude}`;
-    fs.writeFile("./coords", content, function(err) {
+    fs.writeFile((path.join(__dirname,'/','coords')), content, function(err) {
       if(err) {
         return console.log(err);
       }
